cd0654ab4c9f0bd4d6608026c0189c77c1f7b86d
JordanSerafini <jordanserafini.74@gmail.com>
2024-02-22 16:11:22 +0100

    :construction: add tailwind config

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -87,26 +87,30 @@ const marieTotal = marieExpenses.reduce((acc, expense) => acc + expense.amount, 
   return (
     <div className="p-4 h-screen w-full">
       <div className="flex flex-col  h-9/10 justify-stard">
-        <h2 className="text-xl font-bold mb-4 border-b-2 border-c2 pb-4 ">Dépenses</h2>
+        <h2 className="text-xl font-bold mb-4 border-b-2 border-c2 pb-4 text-c2">Dépenses</h2>
         
+        {/* Affiche les dépenses par utilisateur */}
         <div className="flex flex-row justify-evenly p-2 h-4/5 gap-6">
           {users.map((user) => (
             <div key={user.id} className="h-full flex flex-col bg-white border-2 gap-2 w-2/3 p-2">
+              {/* Affiche le nom de l'utilisateur */}
               <h3 className="text-lg font-semibold mb-2">{user.name}</h3>
               <div className="flex flex-col overflow-auto h-full gap-8 justify-start">
+                {/* Affiche MAP dépenses de l'utilisateur par ID*/}
                 {expensesByUser[user.id]?.map((expense) => (
-                  <div key={expense.id} className="text-xs flex flex-col gap-2 mb-3 p-2 shadow rounded border-r-2 border-spacing-2 bg-quaternary  font-Dancing ">
+                  <div key={expense.id} className="text-xs flex flex-row gap-2 mb-3 p-2 shadow rounded border-r-2 border-spacing-2 bg-quaternary  font-Dancing bg-c1 text-c4 items-center justify-evenly">
                     <p className="font-semibold">{expense.name}</p>
                     <p>
                       {expense.amount}€ -{" "}
                       {new Date(expense.date).toLocaleDateString("fr-FR")}
                     </p>
                     <p>{expense.category}</p>
+                    {/* Affiche le bouton pour supprimer une dépense */}
                     <button
                       onClick={() => deleteExpense(expense.id)}
                       className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
                     >
-                      Supprimer
+                      X
                     </button>
                   </div>
                 )) || <p>Cet utilisateur n'a pas de dépenses enregistrées.</p>}
@@ -114,13 +118,16 @@ const marieTotal = marieExpenses.reduce((acc, expense) => acc + expense.amount, 
             </div>
           ))}
         </div>
+        {/* Affiche la différence de dépenses entre Jordan et Marie */}
         <div className="text-center">
         {jordanTotal > marieTotal ? <p>Jordan a dépensé {jordanTotal - marieTotal}€ de plus que Marie</p> : <p>Marie a dépensé {marieTotal - jordanTotal}€ de plus que Jordan</p>}
         </div>
       </div>
+
+      {/* Bouton pour ajouter une dépense */}
       <button
         onClick={() => setShowModal(true)}
-        className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Ajouter une dépense
       </button>
