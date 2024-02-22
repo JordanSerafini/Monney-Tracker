e45c7100787a6eeb4bf48cd1dd44cb4dc88076f9
JordanSerafini <jordanserafini.74@gmail.com>
2024-02-22 08:56:50 +0100

    :construction: add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,50 +30,36 @@ export default {
         '2/10': '20%',
         '3/10': '30%',
         '4/10': '40%',
+        '4.5/10': '45%',
         '5/10': '50%',
         '6/10': '60%',
         '7/10': '70%',
         '8/10': '80%',
+        '8.5/10': '85%',
         '9/10': '90%',
         '9.5/10': '95%',
-
         '10/10': '100%',
       },
-
-      colors: {
-        'yellow-past': '#F7F6CF',
-        'blue-light': '#B6D8F2',
-        'purpl-light': '#F4CFDF',
-        'blue-strong': '#5784BA',
-        'blue': '#9AC8EB',
-
-        '1c': '#18534F',
-        '2c': '#226D68',
-        '3c': '#ECF8F6',
-        '4c': '#FEEAA1',
-        '5c': '#D6955B',
-
-        '6c': '#137C8B',
-        '7c': '#709CA7',
-        '8c': '#B8CBD0',
-        '9c': '#7A90A4',
-        '10c': '#344D59',
-
+      fontSize: {
+        'xxs': '.5rem',
+        'xxxs': '.4rem',
       },
+      colors: {
+        'primary': '#F7F6CF',
+        'secondary': '#B6D8F2',
+        'tertiary': '#F4CFDF',
+        'quaternary': '#5784BA',
+        'quinary': '#9AC8EB',
+      },
+
       fontFamily: {
         'lato': ['Lato', 'sans-serif'],
         'playfair': ['Playfair Display', 'serif'],
       },
-      screens: {
-        'sm': '330px',
-        'w600': '600px',
-
-      },
-      
     },
   },
-  plugins: [
-    
-
-  ],
+  
+  plugins: [],
 }
+
+
