6ea36c2e7540af05028f7111303dad387bb4467c
JordanSerafini <jordanserafini.74@gmail.com>
2024-03-02 21:27:19 +0100

    :construction: working

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -113,6 +113,8 @@ const marieTotal = marieExpenses.reduce((acc, expense) => acc + expense.amount, 
                       onClick={() => deleteExpense(expense.id)}
                       className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
                     >
+
+                      
                       X
                     </button>
                   </div>
