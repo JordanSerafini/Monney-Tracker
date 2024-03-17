3d2f312b28aacefcab9afd0a7af878e3e89be3ef
Jordan S <jordanserafini.74@gmail.com>
2024-03-17 19:31:41 +0100

    :construction:

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -173,6 +173,7 @@ function Main() {
             onClick={() => setShowModal(true)}
             className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/2"
           >
+            
             Ajouter une dépense
           </button>
         </section>
